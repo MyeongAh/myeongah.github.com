01cf1b52fee04b8087280d6e10525484285a8e39
MyeongAh <mohyun1004@ajou.ac.kr>
2016-10-31 19:30:14 +0900

    Disabled Contact Form
    DIsabled Contact Form

--- js/22labjs.js
+++ js/22labjs.js
@@ -63,32 +63,32 @@ $(function scrolly(){
 /* end of Skill Gauge */
 
 /* Contact Form */
-var form = document.getElementsByTagName('form')[0];
-form.addEventListener('submit',contact,false);
-function contact(e) {
+//var form = document.getElementsByTagName('form')[0];
+//form.addEventListener('submit',contact,false);
+//function contact(e) {
    // Prevent Default Form Submission
-   e.preventDefault();
+//   e.preventDefault();
 
-   var target = e.target || e.srcElement;
-   var i = 0;
-   var message = '';
+//   var target = e.target || e.srcElement;
+//   var i = 0;
+//   var message = '';
 
    // Loop Through All Input Fields
-   for(i = 0; i < target.length; ++i) {
+//   for(i = 0; i < target.length; ++i) {
       // Check to make sure it's a value. Don't need to include Buttons
-      if(target[i].type != 'text' && target[i].type != 'textarea') {
+//      if(target[i].type != 'text' && target[i].type != 'textarea') {
              // Skip to next input since this one doesn't match our rules
-         continue;
-      }
+//         continue;
+//      }
 
       // Add Input Name and value followed by a line break
-      message += target[i].name + ': ' + target[i].value + "\r\n";
-   }
+//      message += target[i].name + ': ' + target[i].value + "\r\n";
+//   }
    // Modify the hidden body input field that is required for the mailto: scheme
-   target.elements["body"].value = message;
+//   target.elements["body"].value = message;
 
    // Submit the form since we previously stopped it. May cause recursive loop in some browsers? Should research this.
-   this.submit();
-}
+//   this.submit();
+//}
 
 /* end of Contact Form */
